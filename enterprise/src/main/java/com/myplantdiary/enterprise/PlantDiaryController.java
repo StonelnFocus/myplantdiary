c800f8267581482cfb7b7b20e2dc159dcbbd24c8
Thomas Mahoney <tom8mahoney@gmail.com>
2021-09-14 22:38:54 -0400

    Initial commit

---
+++ enterprise/src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
@@ -0,0 +1,37 @@
+package com.myplantdiary.enterprise;
+
+import com.myplantdiary.enterprise.dto.Specimen;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PlantDiaryController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "start";
+    }
+
+    @GetMapping("/specimen")
+    public ResponseEntity fetchAllSpecimen() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/specimen/id/")
+    public ResponseEntity fetchSpecimenById(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/specimen", consumes="application/json", produces = "application/json")
+    public Specimen createSpecimen(@RequestBody Specimen specimen){
+        return specimen;
+    }
+
+    @DeleteMapping("/specimen/{id}/")
+    public ResponseEntity deleteSpecimen(@PathVariable("id") String id){
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
